79a1b87f16f76153a2506d8ad8ec0d6a8642eb74
Daniel Chung <pikachungg@gmail.com>
2021-03-24 03:24:36 -0400

    updated read me and weird stuff happening

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-"# twitch-spotify-remote" 
-https://developer.spotify.com/documentation/web-api/reference/#category-player
-https://dev.twitch.tv/docs/irc
+# twitch-spotify-remote 
+
+
+Hmm Spotify api is very confusing still have to read through it. 
+For now, you must change manually the code from the accesstoken. 
+It's kinda weird because the accesstoken you get doesn't run the 
+queue ??? because of no auth apparently. 
--- spotify.js
+++ spotify.js
@@ -3,6 +3,7 @@ const client_secret = 'b325991c21a245bf9731ba3563feb41e';
 const redirect_uri = 'https://www.google.com';
 const SpotifyWebApi = require('spotify-web-api-node');
 const fetch = require("node-fetch");
+const open = require("open");
 
 const spotifyApi = new SpotifyWebApi({
     clientId: client_id,
@@ -10,9 +11,15 @@ const spotifyApi = new SpotifyWebApi({
     redirectUri: redirect_uri
 });
 
-spotifyApi.setAccessToken('BQBpS_oHPcfapzpmcnL-Wv_WZVBbsTi35kMlfcStAuCJa4KKJpgL03LCg3UX47lTBlNivsL7j2BoK8FntyTpzH8U4OCTlfRj_SQuuaP5_Dd54FBL5lZiek9StDijQhyQ9_HbQeRN-dfWXFibCZzR_n1ltQ');
+
+spotifyApi.setAccessToken("BQCB7OfzpdGrg7J3MVvvIQW-N52e8Mxdjnba8JHFaNu7KSA4P54E_xgzlqW9ZYEn50Y_wX2Q7Cy3YCeNb_YMld6vF0Zlg7xPA5wTbgpKibtEzWHC1MCSnk2G9FE2_SW0QiWEW49vrBwtGRhkJ-ACliXK7w");
+
+const getToken = () => {
+    open("https://developer.spotify.com/console/post-queue/?uri=spotify%3Atrack%3A3CeCwYWvdfXbZLXFhBrbnf&device_id=1c99930054fb8a842c366f0936cdd1a8278bbfc0")
+}
 
 const addToQueue = (songName) => {
+
     let returnVal = true;
 
     spotifyApi.searchTracks('track:' + songName)
@@ -30,14 +37,20 @@ const addToQueue = (songName) => {
 }
 
 const requestQueue = (id) => {
+    let token = spotifyApi.getAccessToken();
+    console.log(token);
     fetch('https://api.spotify.com/v1/me/player/queue?uri=spotify%3Atrack%3A' + id + '&device_id=1c99930054fb8a842c366f0936cdd1a8278bbfc0', {
         method: 'POST',
         headers: {
             "Accept": "application/json",
             "Content-Type": "application/json",
-            "Authorization": "Bearer "  + "BQBpS_oHPcfapzpmcnL-Wv_WZVBbsTi35kMlfcStAuCJa4KKJpgL03LCg3UX47lTBlNivsL7j2BoK8FntyTpzH8U4OCTlfRj_SQuuaP5_Dd54FBL5lZiek9StDijQhyQ9_HbQeRN-dfWXFibCZzR_n1ltQ"
+            "Authorization": "Bearer "  + token
         }
     })
+    .then(response => {
+        console.log(response);
+    })
+    .catch( err => console.log(err))
 }
 
 module.exports = {addToQueue};
